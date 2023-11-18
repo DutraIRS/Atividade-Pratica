ef7555a727dfa4fd06d94e57e4c6f623b62cdb80
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-11-18 13:43:42 -0300

    Update input_receiver.py
    Código para passar nos testes

--- src/input_receiver.py
+++ src/input_receiver.py
@@ -1,2 +1,25 @@
 class InputReceiver:
-    ...
+    # Single-responsability: receive input
+    # Open-closed: can be extended by InputValidator, but not modified
+
+    def __init__(self, validator: "InputValidator") -> None:
+        self._input_received = None
+        self.validator = validator
+
+    @property
+    def input_received(self):
+        return self._input_received
+    
+    @input_received.setter
+    def input_received(self, value: str) -> None:
+        self._input_received = value
+
+    def receive_input(self) -> None:
+        input_received = input("Digite o CPF: ")
+
+        while not self.validator.validate(input_received):
+            print("CPF inválido")
+            print("Formato para inserir CPF -", self.validator.expected_format())
+            input_received = input("Digite o CPF: ")
+
+        self.input_received = input_received
