b675d57e561056ba162385a2179a7bbee4709335
LuisHDBueno <105614892+LuisHDBueno@users.noreply.github.com>
2023-11-30 21:25:04 -0300

    Document the input receiver

--- src/input_receiver.py
+++ src/input_receiver.py
@@ -1,26 +1,47 @@
 from validator import Validator
 
 class InputReceiver:
-    # Single-responsability: receive input
-    # Open-closed: can be extended by InputValidator, but not modified
+    """ Single-responsability: Interact with the user
+
+    Open-closed: can be extended by InputValidator, but not modified
+    """
 
     def __init__(self, *validator: [Validator]) -> None:
+        """ Class to interact with the user
+
+        :param validator: All validators to be used to validate the input
+        :type validator: [Validator]
+        """        
         self._input_received = None
         self.validators = validator
 
     @property
-    def input_received(self):
+    def input_received(self) -> str:
+        """ Getter for the input received
+
+        :return: Input received
+        :rtype: str
+        """        
         return self._input_received
     
     @input_received.setter
     def input_received(self, value: str) -> None:
+        """ Setter for the input received
+
+        :param value: Input received
+        :type value: str
+        """        
         self._input_received = value
 
     def receive_input(self) -> None:
+        """ Function to interact with the user
+        """        
         input_received = input("Digite o CPF: ")
 
         valid = False
+        # Loop until cpf received is valid
         while not valid:
+            # Loop through all validators
             for each_validator in self.validators:
                 valid = each_validator.validate(input_received)
 
