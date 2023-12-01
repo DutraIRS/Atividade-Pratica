ed64dcc7e90833615d21b6666506ac50b2a500ca
Anajuliarlc <anajuliaritalc@gmail.com>
2023-11-30 22:00:14 -0300

    Document test input validator

--- test/test_input_validator.py
+++ test/test_input_validator.py
@@ -5,32 +5,49 @@ import input_validator
 import unittest
 
 class TestInputValidator(unittest.TestCase):
-    # Equivalence partitioning: invalid inputs can be divided into:
-    #   - wrong length
-    #   - missing punctuation
-    #   - wrong punctuation
-    #   - non-digit characters
+    """ All tests for the InputValidator class
+    """
     def setUp(self):
+        """ Set up the test case by instantiating the InputValidator class
+        """        
         self.validator = input_validator.InputValidator()
 
     def test_validate_valid_input(self):
+        """ Test InputValidator.validate() with a valid CPF
+        """        
         self.assertTrue(self.validator.validate('123.456.789-00'))
 
     def test_validate_wrong_length(self):
+        """ Test InputValidator.validate() with a CPF with the wrong length
+        """
+        # Test with 12 digits        
         self.assertFalse(self.validator.validate('123.456.789-000'))
+        # Test with 10 digits
         self.assertFalse(self.validator.validate('123.456.789-0'))
 
     def test_validate_missing_punctuation(self):
+        """ Test InputValidator.validate() with a CPF with missing punctuation
+        """
+        # Test with missing hyphens
         self.assertFalse(self.validator.validate('123.456.78900'))
+        # Test with missing dots
         self.assertFalse(self.validator.validate('123456.789-00'))
         self.assertFalse(self.validator.validate('123.456789-00'))
 
     def test_validate_wrong_punctuation(self):
+        """ Test InputValidator.validate() with a CPF with wrong punctuation
+        """
+        # Test with hyphens instead of dots
         self.assertFalse(self.validator.validate('123-456-789-00'))
+        # Test with dots instead of hyphens
         self.assertFalse(self.validator.validate('123.456.789.00'))
+        # Test with commas instead of dots
         self.assertFalse(self.validator.validate('123,456,789,00'))
 
     def test_validate_non_digit_characters(self):
+        """ Test InputValidator.validate() with a CPF with non-digit characters
+        """
+        # Test with non digit in each block
         self.assertFalse(self.validator.validate('123.456.789-0X'))
         self.assertFalse(self.validator.validate('123.456.78X-00'))
         self.assertFalse(self.validator.validate('123.45X.789-00'))
